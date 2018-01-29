Bug fixes: - Incorrect decoder layer sizes - Inplace gradient override - Reconstruction loss misuse

data_loader.py
@@ -1,7 +1,6 @@
 import os
 import torch
 from torch.utils.data import Dataset
-from torch.autograd import Variable
 
 
 class UMDDataset(Dataset):
@@ -56,8 +55,7 @@ class UMDDataset(Dataset):
 
     @staticmethod
     def to_one_hot(y, dof=180, dof_quant=1):
-        y_tensor = y.data if isinstance(y, Variable) else y
-        y_tensor = y_tensor.div_(dof_quant).type(torch.LongTensor).view(-1, 1)
+        y_tensor = y.div_(dof_quant).type(torch.LongTensor).view(-1, 1)
         y_one_hot = torch.zeros(y_tensor.size()[0], dof).scatter_(1, y_tensor, 1)
         return y_one_hot.view(-1)
 

model.py
@@ -2,6 +2,7 @@ import torch.nn as nn
 import torch
 
 MAX_NUM_FEATURES = 512  # Maximum depth (k) in Encoder / Decoder / Discriminator convolution block Ck
+IMAGE_DIMENSIONS = 3    # RGB
 
 
 def xavier_initialization(modules):
@@ -19,7 +20,7 @@ class FaderNetAutoencoder(nn.Module):
     @staticmethod
     def create_encoder_blocks(num_of_layers):
 
-        in_channels = 3    # RGB
+        in_channels = IMAGE_DIMENSIONS  # Normally RGB
         out_channels = 16
         neural_net = []
 
@@ -44,14 +45,14 @@ class FaderNetAutoencoder(nn.Module):
 
         for i in range(1, num_of_layers+1):
             neural_net.append(nn.Sequential(
-                nn.ConvTranspose2d(in_channels=in_channels+attr_dim, out_channels=out_channels+attr_dim,
+                nn.ConvTranspose2d(in_channels=in_channels+attr_dim, out_channels=out_channels,
                                    kernel_size=4, stride=2, padding=1, bias=False),
                 nn.BatchNorm2d(num_features=out_channels),
                 nn.ReLU(inplace=True)
             ))
 
             in_channels = out_channels
-            out_channels = min(2**(2+num_of_layers-i), MAX_NUM_FEATURES)
+            out_channels = IMAGE_DIMENSIONS if (i == num_of_layers-1) else min(2**(2+num_of_layers-i), MAX_NUM_FEATURES)
 
         return nn.ModuleList(neural_net)
 
@@ -72,8 +73,10 @@ class FaderNetAutoencoder(nn.Module):
 
     def decode(self, z, y):
         x_decoded = z
+        y_reshape = y.unsqueeze(2).unsqueeze(3)
         for layer in self.decoder_layers:
-            x_decoded = torch.cat([x_decoded, y], dim=1)
+            y_expanded = y_reshape.expand(-1, -1, x_decoded.shape[2], x_decoded.shape[3])
+            x_decoded = torch.cat([x_decoded, y_expanded], dim=1)
             x_decoded = layer(x_decoded)
 
         return x_decoded
@@ -95,7 +98,7 @@ class FaderNetDiscriminator(nn.Module):
             nn.Conv2d(in_channels=block_size, out_channels=block_size, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm2d(num_features=block_size),
             nn.LeakyReLU(negative_slope=0.2, inplace=True),
-            nn.Dropout(p=0.3, inplace=True)
+            nn.Dropout(p=0.3)
         )
 
         self.proj = nn.Sequential(

trainer.py
@@ -3,9 +3,9 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from torch.autograd import Variable
-from FaceRotNet.data_loader import UMDDataset
-from FaceRotNet.plotter import Plotter
-from FaceRotNet.model import FaderNetAutoencoder, FaderNetDiscriminator
+from data_loader import UMDDataset
+from plotter import Plotter
+from model import FaderNetAutoencoder, FaderNetDiscriminator
 import os
 
 
@@ -52,27 +52,27 @@ class FaderNetTrainer:
             batch_size = y.size()[0]
             degs_dim = self.t_params['deg_dim']
 
-            for angle_idx in range(0, 3 * degs_dim, degs_dim):
+            for angle_idx in range(0, 3*degs_dim, degs_dim):
                 y_target = y[:, angle_idx:angle_idx+degs_dim].max(1)[1]  # Index of target degree
                 delta = torch.LongTensor(batch_size).random_(degs_dim - 1) + 1
                 if self.use_cuda:
                     delta.cuda()
                 y_target = (y_target + Variable(delta)) % degs_dim
                 y_predict_target = y_predict[:, angle_idx:angle_idx+degs_dim]
-                loss = loss + self.adversarial_loss_func(y_predict_target, Variable(y_target))
+                loss = loss + self.adversarial_loss_func(y_predict_target, y_target)
         else:
             loss = loss + Variable(self.max_regress_loss) - self.adversarial_loss_func(Variable(y_predict), Variable(y))
 
         return loss
 
-    def reconstruct_loss(self, x, x_pred):
-        return self.self.reconstruction_loss_func(x, x_pred)
+    def reconstruct_loss(self, x, x_reconstruct):
+        return self.reconstruction_loss_func(x_reconstruct, x)
 
     def discr_iteration(self, batch):
         self.autoenc.eval()
         self.discrm.train()
 
-        x = Variable(batch['data'])
+        x = Variable(batch['data'], requires_grad=False)
         y = batch['label']
 
         if self.use_cuda:
@@ -95,13 +95,14 @@ class FaderNetTrainer:
         self.autoenc.train()
         self.discrm.eval()
 
-        x = Variable(batch['data'])
-        y = batch['label']
+        x = Variable(batch['data'], requires_grad=False)
+        y = Variable(batch['label'])
 
         if self.use_cuda:
             x.cuda()
+            y.cuda()
 
-        z, x_reconstruct = self.autoenc(x)
+        z, x_reconstruct = self.autoenc(x, y)
 
         with torch.no_grad():
             y_predict = self.discrm(z)